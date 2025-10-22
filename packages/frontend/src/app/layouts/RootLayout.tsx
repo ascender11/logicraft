98fc4e11b88d454368fe43075bf9817adfba1105
Anton <ant.radkov@gmail.com>
2025-10-22 10:21:41 +0300

    refactor(layouts): integrate Header into RootLayout

--- packages/frontend/src/app/layouts/RootLayout.tsx
+++ packages/frontend/src/app/layouts/RootLayout.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router-dom';
 
+import { Header } from '@/widgets/header';
+
 export const RootLayout = () => {
   return (
     <div className='flex h-screen flex-col'>
-      <header />
+      <Header className='z-10' />
       <main className='flex-1 overflow-hidden'>
         <Outlet />
       </main>
